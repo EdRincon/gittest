f76f445b584e78cb413dd7ae3889c26084774c12
EdgarR <EdgarR@erincon.trivius.com.mx>
2016-05-02 22:02:51 -0500

    Primer commit

---
+++ GitTest/src/main/java/EdRincon/GitTest/App.java
@@ -0,0 +1,40 @@
+package EdRincon.GitTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ */
+
+public class App 
+{
+	protected WebDriver driver;
+	
+	@Test
+	public void tutorial() throws InterruptedException{
+		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		String eTitle = "Meet Guru99";
+		String aTitle = "";
+		//
+		//Launch browser and go to URL
+		//
+		driver.get("http://www.guru99.com/");
+		//Maximizes the browser window
+		driver.manage().window().maximize();
+		//Get the actual value of the title
+		aTitle = driver.getTitle();
+		//Compare the actual title with the expected title
+		if (aTitle.contentEquals(eTitle)){
+			System.out.println("Test Passed");
+		} else{
+			System.out.println("Test Failed");
+		}
+		//Close browser
+		//driver.close();
+		driver.quit();
+	}
+	
+}
